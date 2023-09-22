da381b2a5e5ff22cb789b67300c018098c319dbf
sourabh <sourabh.mittal02@gmail.com>
2023-09-22 13:17:52 +0530

    change

--- Models/ViewModel/ProductViewAPIModel.cs
+++ Models/ViewModel/ProductViewAPIModel.cs
@@ -2,11 +2,13 @@
 {
     public class ProductViewAPIModel
     {
+        public Int64 PID { get; set; }
         public string PName { get; set; } = string.Empty;
         public bool Is_Veg { get; set; } = false;
         public bool Is_Alcoholic { get; set; } = false;
         public string Image { get; set; } = string.Empty;
         public string CategoryName { get; set; } = string.Empty;
         public Int64 OrderedQuntity { get; set; } 
+        public decimal PRate { get; set; } 
     }
 }
