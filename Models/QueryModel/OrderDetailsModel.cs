ab441f0b17c29275c8aba825313225d79eba7716
ajayPrismHR <107325460+ajayPrismHR@users.noreply.github.com>
2023-10-19 10:09:17 +0530

    Order

---
+++ Models/QueryModel/OrderDetailsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RMS_API.Models.QueryModel
+{
+    public class OrderDetailsModel
+    {
+        public string OrderID {  get; set; } 
+        public string ProductID { get; set; }
+        public string OfferID { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        
+    }
+}
